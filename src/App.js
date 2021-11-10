06c1bd17b595267af72ac6ac34af311ccea9f049
Genie <genie0320@gmail.com>
2021-11-10 12:00:14 +0900

    func-Card Matching

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SingleCard from './SingleCard';
 
 const cardImgs = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" }
+  { "src": "/img/helmet-1.png", "matched": "false" },
+  { "src": "/img/potion-1.png", "matched": "false" },
+  { "src": "/img/ring-1.png", "matched": "false" },
+  { "src": "/img/scroll-1.png", "matched": "false" },
+  { "src": "/img/shield-1.png", "matched": "false" },
+  { "src": "/img/sword-1.png", "matched": "false" }
 ]
 
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [selection01, setSelection01] = useState(null)
+  const [selection02, setSelection02] = useState(null)
 
   const shuffle = () => {
     const cardList = [...cardImgs, ...cardImgs]
@@ -22,18 +24,53 @@ function App() {
       .map((card) => ({ ...card, id: Math.random() }))
     console.clear()
     setCards(cardList)
-    setTurns(0)
+    setTurns(1)
   }
 
-  console.log(cards, turns)
+  // User choice
+  const choiceCard = (cardInfo) => {
+    selection01 ? setSelection02(cardInfo) : setSelection01(cardInfo)
+    // console.log("sel01 : ", selection01)
+  }
 
+  // compare cards
+  useEffect(() => {
+    if (selection01 && selection02) {
+
+      if (selection01.src === selection02.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === selection01.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        console.log('match!', turns)
+        resetCard()
+      } else {
+        console.log('not match', turns)
+        resetCard()
+      }
+
+      console.log(cards, turns)
+    }
+  }, [selection01, selection02])
+
+  // card reset
+  const resetCard = () => {
+    setSelection01(null)
+    setSelection02(null)
+    setTurns(turns => turns + 1)
+  }
   return (
     <div className="App">
       <h1 className="genieTest">genitest</h1>
       <button onClick={shuffle}>New Game</button>
       <div className="card-spread">
         {cards.map(card => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard key={card.id} card={card} choiceCard={choiceCard} />
         ))}
       </div>
     </div>
--- src/SingleCard.js
+++ src/SingleCard.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './SingleCard.css'
 
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, choiceCard }) {
+
+    const cardClick = () => {
+        choiceCard(card)
+    }
     return (
         <>
             <div className="card">
-                <img className="back" src="/img/cover.png" alt="Card back" />
+                <img className="back" src="/img/cover.png" onClick={cardClick} alt="Card back" />
                 <img className="front" src={card.src} alt="Card front" />
             </div>
         </>
